007c7fd30882c82bab4f8593f91dc7d4c7ceefc0
protoEvangelion <ryantgarant@gmail.com>
2017-08-25 14:16:37 -0700

    Fixed current branch script

--- linux/dotfiles/git.plugin.zsh
+++ linux/dotfiles/git.plugin.zsh
@@ -252,3 +252,11 @@ function work_in_progress() {
     echo "WIP!!"
   fi
 }
+
+# The name of the current branch
+# Back-compatibility wrapper for when this function was defined here in
+# the plugin, before being pulled in to core lib/git.zsh as git_current_branch()
+# to fix the core -> git plugin dependency.
+function current_branch() {
+  git_current_branch
+}
