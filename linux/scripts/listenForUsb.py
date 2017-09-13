8c12b7a5825e8cece89e6765ef97281d24585435
protoEvangelion <ryantgarant@gmail.com>
2017-09-13 10:05:25 -0700

    Added python script to listen for usb insertions

---
+++ linux/scripts/listenForUsb.py
@@ -0,0 +1,27 @@
+import glib
+import os
+
+from pyudev import Context, Monitor
+
+try:
+    from pyudev.glib import MonitorObserver
+
+    def device_event(observer, device):
+        print 'TRY event {0} on device {1}'.format(device.action, device)
+	os.system("xmodmap ~/.Xmodmap")
+except:
+    from pyudev.glib import GUDevMonitorObserver as MonitorObserver
+
+    def device_event(observer, action, device):
+        print 'EXCEPT event {0} on device {1}'.format(action, device)
+
+context = Context()
+monitor = Monitor.from_netlink(context)
+
+monitor.filter_by(subsystem='usb')
+observer = MonitorObserver(monitor)
+
+observer.connect('device-event', device_event)
+monitor.start()
+
+glib.MainLoop().run()
