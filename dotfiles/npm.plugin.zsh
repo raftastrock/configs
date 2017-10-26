1b1709d6a44b009191da2c64a0a71a8cb8dd48ae
protoEvangelion <ryantgarant@gmail.com>
2017-10-26 15:31:57 -0700

    master switched to single quotes

--- dotfiles/npm.plugin.zsh
+++ dotfiles/npm.plugin.zsh
@@ -1,5 +1,5 @@
 (( $+commands[npm] )) && {
-		__NPM_COMPLETION_FILE="${ZSH_CACHE_DIR}/npm_completion"
+		__NPM_COMPLETION_FILE='${ZSH_CACHE_DIR}/npm_completion'
 
 		if [[ ! -f $__NPM_COMPLETION_FILE ]]; then
 				npm completion >! $__NPM_COMPLETION_FILE 2>/dev/null
@@ -11,24 +11,23 @@
 		unset __NPM_COMPLETION_FILE
 }
 
-
-alias ng="sudo npm i -g "
-alias nis="npm i -S "
-alias nid="npm i -D "
-alias no="npm outdated"
-alias nl="npm list"
+alias ng='sudo npm i -g '
+alias nis='npm i -S '
+alias nid='npm i -D '
+alias no='npm outdated'
+alias nl='npm list'
 alias nlink='sudo npm link'
 alias nlocation='sudo npm -g list --depth=0'
-alias nr="npm run "
-alias ns="npm start"
-alias nt="npm test"
+alias nr='npm run '
+alias ns='npm start'
+alias nt='npm test'
 
-alias yg="sudo yarn i -g "
-alias yis="yarn i -S "
-alias yid="yarn i -D "
-alias yr="yarn run "
-alias ys="yarn start"
-alias yt="yarn test"
+alias yg='sudo yarn i -g '
+alias yis='yarn i -S '
+alias yid='yarn i -D '
+alias yr='yarn run '
+alias ys='yarn start'
+alias yt='yarn test'
 
 function nsv(){
 	npm show $1 versions
