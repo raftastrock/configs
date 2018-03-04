9ebd21cb58a072e67ec346ec12450da2a685983d
protoEvangelion <ryantgarant@gmail.com>
2018-03-03 19:26:07 -0800

    Add yarn to bootstrap software

--- scripts/bootstrapLinuxImage.sh
+++ scripts/bootstrapLinuxImage.sh
@@ -109,6 +109,8 @@ vsCodeFedora() {
 
 checkSoftware "code" "vsCodeDebian" "vsCodeFedora"
 
+checkSoftware "yarn" "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo \"deb https://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt update && sudo apt -y install yarn" "curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo && sudo dnf -y install yarn"
+
 checkSoftware "node" "curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - && sudo apt -y install nodejs" "curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash - && sudo dnf -y install nodejs"
 
 echo -e "$blu Updating/Installing my fav node packages globally $off"
