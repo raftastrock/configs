63340f526b9f48fd28e11f8d915c28777d5406db
protoEvangelion <ryantgarant@gmail.com>
2018-04-18 16:44:56 -0700

    Add curlFiles python script

---
+++ scripts/curlFiles.py
@@ -0,0 +1,32 @@
+import json
+import subprocess
+import socket
+import socks
+import urllib.request
+
+
+def main():
+    jsonFile = open("stuff.json", "r")
+
+    if jsonFile.mode == "r":
+        obj = json.load(jsonFile)
+
+        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
+        socket.socket = socks.socksocket
+
+        index = 1
+
+        for key, value in obj.items():
+            print("i:", index, key, value, "\n")
+
+            urllib.request.FancyURLopener().retrieve(
+                value, '/home/ryan/Videos/FrontEnd/4SemestersOfCompSciPartI/' + str(index) + "_" + key + '.webm')
+
+            index = index + 1
+
+    else:
+        print("hello")
+
+
+if __name__ == "__main__":
+    main()
